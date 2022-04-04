c59a28010399bcad63cffaa0b282e69cfb6049e3
Pranit Patil <dev.pranitpatil@gmail.com>
2022-04-04 16:36:18 +0530

    database changed setings

--- CollegeForum/settings.py
+++ CollegeForum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9u)9j6a*b6lh(pt5i%t=drr7@z#(^m5yw05&1x&d7^#pvk8q_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['collegeforumacpce.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
