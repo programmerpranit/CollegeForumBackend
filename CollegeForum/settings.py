69394b36e92c6cf8c51e2dabe8c16cd55b61bdba
Pranit Patil <dev.pranitpatil@gmail.com>
2022-04-04 21:36:48 +0530

    localdb

--- CollegeForum/settings.py
+++ CollegeForum/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'CollegeForum.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'd74m54jfrq3bmf',
-       'USER': 'asystlpvsilgkh',
-       'PASSWORD': '04ad18cead135bcc2e85c1728203e3e48151715110384a7cf42090b7fda3cdac',
-       'HOST': 'ec2-34-197-84-74.compute-1.amazonaws.com',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd74m54jfrq3bmf',
+#         'USER': 'asystlpvsilgkh',
+#         'PASSWORD': '04ad18cead135bcc2e85c1728203e3e48151715110384a7cf42090b7fda3cdac',
+#         'HOST': 'ec2-34-197-84-74.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # DATABASES = {
@@ -105,17 +105,16 @@ DATABASES = {
 #         'CLIENT': {
 #                 'host': 'mongodb+srv://college:25Q9BLe8XVAMFNzv@forum-cluster.x7jxl.mongodb.net/college-forum?retryWrites=true&w=majority',
 #                 'authMechanism':'SCRAM-SHA-1',
-#             }  
+#             }
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from djongo import models
 from account.models import User
 
 # Create your models here.
