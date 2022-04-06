1e8883cbc58d7929a34ef4cc753d1ddb86d3bba8
Pranit Patil <dev.pranitpatil@gmail.com>
2022-04-06 23:18:22 +0530

    new one

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     uid = models.BigAutoField(primary_key=True)
     sub = models.CharField(max_length=500, unique=True) 
-    email = models.EmailField(max_length=255, unique=True)
+    useremail = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255, null=True, default="")
     year_of_study = models.PositiveIntegerField(null=True, default=1)
     prn = models.IntegerField(null=True, unique=True)
